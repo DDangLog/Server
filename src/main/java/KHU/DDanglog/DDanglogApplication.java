8d24104ba7837da1e837f0d8a316e29f85058413
seungheon123 <seungheon.bisb.2012@gmail.com>
2023-09-18 16:47:20 +0900

    초기 설정

---
+++ src/main/java/KHU/DDanglog/DDanglogApplication.java
@@ -0,0 +1,13 @@
+package KHU.DDanglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDanglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DDanglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KHU/DDanglog/DDanglogApplicationTests.java
@@ -0,0 +1,13 @@
+package KHU.DDanglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DDanglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
